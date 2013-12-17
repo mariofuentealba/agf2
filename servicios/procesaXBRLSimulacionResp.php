39d3f3005f5f7f51fd2cea31bcf65ee69b4642ce
mariofuentealba <anti.demiurgo@gmail.com>
2013-12-17 08:17:04 -0200

    corrigiendo ingreso masivo

--- servicios/procesaXBRLSimulacionResp.php
+++ servicios/procesaXBRLSimulacionResp.php
@@ -16,34 +16,44 @@ $str = file_get_contents("data.txt");
 $arr = explode("\n", $str);
 $n = count($arr);
 echo "N = " . $n . "<br />";
+
+$arrTotal = array();
+$arrTotal[0] = array();
+$y = 0;
 for($i = 0; $i < $n; $i++){
+	if(trim($arr[$i]) == "END"){
+		//$arrTotal[++$y][count($arrTotal[$y])] = $arr[$i];;
+		$y++;
+		$arrTotal[$y] =array();
+	} else {
+		$arrTotal[$y][count($arrTotal[$y])] = $arr[$i];
+	}
 	
-	$ind = trim($arr[$i++]);
+}
+print_r($arrTotal);
+//exit();
+
+for($i = 0; $i < $y; $i++){
+	$ind = $arrTotal[$i][0];
 	$mysqli->query("INSERT INTO tag_agf value (null, '" . $ind ."', '" . $ind ."', 'NO ESPECIFICA', 'XBRL', 1)");
 	$tag_agf = $mysqli->insert_id;	
-	$stop = 0;
-	$s = 0;
 	$e = 0;
-	$line = "";
-	while($line != "END"){
-		echo $arr[$i]."<br />";
-		$arr[$i] = trim($arr[$i]);
-		$line = $arr[$i];
-		$arr[$i] = explode(";", $arr[$i]);
-		
-		for($j = 0; $j < count($arr[$i]); $j++){
-	
-			$arr[$i][$j] = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`) 
-							VALUES (null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$s] . ", " . ($arr[$i][$j] == '' ? 0 : $arr[$i][$j]) . ", null, 1);";
-			$sql2 = str_replace("'", "''", $arr[$i][$j]);
+	//$s = 0;
+	for($j = 1; $j < count($arrTotal[$i]); $j++, $e++){
+		$line = explode(";", $arrTotal[$i][$j]);
+		print_r($line);
+		for($x = 0; $x < count($line); $x++){
+			$sql = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`) 
+							VALUES (null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$x] . ", " . ($line[$x] == '' ? 0 : $line[$x]) . ", null, 1);";
+			$sql2 = str_replace("'", "''", $sql);
 			$mysqli->query("INSERT INTO log values ('" . $sql2 . "');");
-			$mysqli->query($arr[$i][$j]);
+			$mysqli->query($sql);
 			
 			
 			
-			$arr[$i][$j] = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`, tipo) 
+			$sql = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`, tipo) 
 			SELECT 
-				null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$s] . ", 
+				null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$x] . ", 
 				SUM(valor), null, 1, 'ACUMULADO'
 			FROM valores x
 			WHERE 	tipo = 'TRIMESTRAL'			
@@ -52,14 +62,14 @@ for($i = 0; $i < $n; $i++){
 									WHERE r.ano = p.ano
 											  AND r.mes <= p.mes
 											  AND r.oa = 1
-											  AND p.id_periodo = " . $per[$s] . ")
+											  AND p.id_periodo = " . $per[$x] . ")
 					AND id_tag_agf = " . $tag_agf . "
 					AND id_empresa = " . $emp[$e] . "
 					;";
-			$sql2 = str_replace("'", "''", $arr[$i][$j]);
+			$sql2 = str_replace("'", "''", $sql);
 			//echo "query 2<br />";
 			//$mysqli->query("INSERT INTO log values ('" . $sql2 . "');");
-			$mysqli->query($arr[$i][$j]);
+			$mysqli->query($sql);
 			
 			$val = $mysqli->insert_id;
 			
@@ -72,7 +82,7 @@ for($i = 0; $i < $n; $i++){
 									WHERE r.ano = p.ano
 											  AND r.mes <= p.mes
 											  AND r.oa = 1
-											  AND p.id_periodo = " . $per[$s] . ")
+											  AND p.id_periodo = " . $per[$x] . ")
 					AND id_tag_agf = " . $tag_agf . "
 					AND id_empresa = " . $emp[$e] . ";";
 			$sql2 = str_replace("'", "''", $sql);
@@ -93,24 +103,24 @@ for($i = 0; $i < $n; $i++){
 			
 			
 			
-			$arr[$i][$j] = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`, tipo) 
-			SELECT null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$s] . ", 
+			$sql = "INSERT INTO valores (`ID_VALOR`, `ID_TAG_AGF`, `ID_EMPRESA`, `ID_PERIODO`, `VALOR`, `DT_MODIFICACION`, `origen`, tipo) 
+			SELECT null, '" . $tag_agf . "', " . $emp[$e] . ", " . $per[$x] . ", 
 				SUM(valor), null, 1, 'ANUAL'
 			FROM valores 
 			WHERE tipo = 'TRIMESTRAL'				
 				AND id_periodo in (SELECT r.id_periodo
 									FROM periodos r
 									WHERE r.oa = 1
-										AND r.orden in (" . ($s + 1) . ", " . ($s + 1) . " - 1, " . ($s + 1) . " - 2, " . ($s + 1) . " - 3)) 
+										AND r.orden in (" . ($x + 1) . ", " . ($x + 1) . " - 1, " . ($x + 1) . " - 2, " . ($x + 1) . " - 3)) 
 				AND id_tag_agf = " . $tag_agf . "
 				AND id_empresa = " . $emp[$e] . "						
 		GROUP BY id_tag_agf, id_empresa, origen				
 					;";
 					
-			$sql2 = str_replace("'", "''", $arr[$i][$j]);
+			$sql2 = str_replace("'", "''", $sql);
 			//echo "query 2<br />";
 			//$mysqli->query("INSERT INTO log values ('" . $sql2 . "');");
-			$mysqli->query($arr[$i][$j]);
+			$mysqli->query($sql);
 			
 			$val = $mysqli->insert_id;
 			
@@ -121,7 +131,7 @@ for($i = 0; $i < $n; $i++){
 				AND id_periodo in (SELECT r.id_periodo
 									FROM periodos r
 									WHERE r.oa = 1
-										AND r.orden in (" . ($s + 1) . ", " . ($s + 1) . " - 1, " . ($s + 1) . " - 2, " . (($s++) + 1) . " - 3)) 
+										AND r.orden in (" . ($x + 1) . ", " . ($x + 1) . " - 1, " . ($x + 1) . " - 2, " . (($x) + 1) . " - 3)) 
 				AND id_tag_agf = " . $tag_agf . "
 				AND id_empresa = " . $emp[$e] . "						
 						
@@ -144,36 +154,17 @@ for($i = 0; $i < $n; $i++){
 			//$mysqli->query("INSERT INTO log values ('" . $sql2 . "');");
 			$mysqli->query($sql2);
 			$mysqli->query($sql);
-			
-			
-			
-			
 		}
-		
-		
-		
-		
-		$e++;
-		$i++;
-		if($e == 6){
-			$e = 0;
-		}
-		
-		$s = 0;
-		
 	}
-	
-//	$m->insertarIndicesFinancieros(array($ind, 0, 0, 0, 0, 0, 0, 0, 0, 2), 'indices_financieros', array(1, 2, 3, 4, 5, 6), 0, array('C1_'), array("" . $tag_agf . ";-1;-1;-1;-1;C;0;0;0;0;C.1.NN;no;no;no;no"));
-	
-	
-	//exit();
-	echo "I = " . $i-- . "<br />";
+	$m->insertarIndicesFinancieros(array($ind, 0, 0, 0, 0, 0, 0, 0, 0, 2), 'indices_financieros', array(1, 2, 3, 4, 5, 6), 0, array('C1_'), array("" . $tag_agf . ";-1;-1;-1;-1;C;0;0;0;0;C.1.NN;no;no;no;no"));
 }
-exit();
+
+
+/*exit();
 if (!$mysqli->commit()) {
     print("Falló la consignación de la transacción\n");
     exit();
-}
+}*/
 
 $mysqli->close();
 
