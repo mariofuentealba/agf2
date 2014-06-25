948067f6357afcdfdccad2306edcef38b1d4b173
mariofuentealba <anti.demiurgo@gmail.com>
2014-06-24 21:53:04 -0400

    Update XbrlCarga.php

--- servicios/XbrlCarga.php
+++ servicios/XbrlCarga.php
@@ -2,10 +2,12 @@
 class XbrlCarga{
 
 	public function insertarValor($xmlParam){//$etiqueta, $valor, $rut, $periodo){
-	
-		try { 
-			$con = new PDO('sqlsrv:Server=WOTAN-PC;Database=agf');	 
-			//$con = new PDO('sqlsrv:Server=MFUENTEALBA\WOTAN;Database=agf');			
+		$log = fopen('xbrl\\log.txt', 'a');
+		fwrite($log, print_r($xmlParam, true) . "\r\n");
+		fclose($log);
+		/*try { 
+			//$con = new PDO('sqlsrv:Server=WOTAN-PC;Database=agf');	 
+			$con = new PDO('sqlsrv:Server=MFUENTEALBA\WOTAN;Database=agf');			
 							
 			try {
 		        $con->beginTransaction(); 
@@ -31,7 +33,7 @@ class XbrlCarga{
 		unset($con); 
 		unset($stmt);
 		
-		
+		*/
 		
 		
 		return "mal 2";//$xmlParam->xmlParam;
@@ -135,7 +137,7 @@ class XbrlCarga{
 	
 	public function recursivo($periodo){
 	
-	
+		
 		
 		ini_set('max_execution_time', 0);
 		
@@ -143,7 +145,8 @@ class XbrlCarga{
 		$per = array($arr[0]);
 		$anio = array($arr[1]);
 		$arrRut = '<Resp>';
-		
+		$xml = "<Ruts>";
+		$rutsProcesados = array();
 		$i = 0;
 		$j = 0;
 		$ArchivoRemoto = "http://www.svs.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=" . $per[$j] . "&aa_ifrs=" . $anio[$i] . "";
@@ -190,9 +193,12 @@ class XbrlCarga{
 				$aux2 =  explode("=", $bufer2[10]);
 				$aux3 = explode("=", $bufer2[11]);
 				$bufer = array($aux[1], $aux1[1], $aux2[1], $aux3[1]);//, explode("=", $bufer2[9])[1], explode("=", $bufer2[10])[1], explode("=", $bufer2[11])[1]);
-				$arrRut .= '<rut>' . $bufer[0] . '</rut>';
+				
 				$url = "http://www.svs.cl/institucional/inc/inf_financiera/ifrs/safec_ifrs_verarchivo.php?auth=&send=&rut=" . $bufer[0] . "&mm=" . $bufer[1] . "&aa=" . $bufer[2] . "&archivo=" . $bufer[0] . "_" . $bufer[2] . $bufer[1] . "_" . $bufer[3] . ".zip&desc_archivo=Estados financieros (XBRL)&tipo_archivo=XBRL";
-			
+				$xml .= '<rut id="' . $bufer[0] . '" url="' . str_replace("&", "::", $url) . '" periodo="' . $periodo . '"/>';
+				$rutsProcesados[count($rutsProcesados)] = $bufer[0];
+				//$xml .= '<rut id="' . $bufer[0] . '" url="' . $url . '" periodo="' . $periodo . '"/>';
+				$url = str_replace("::", "&", $url);
 				fwrite($f, $url . "\r\n");
 				fwrite($resp, $url . "\r\n");		
 				
@@ -208,7 +214,7 @@ class XbrlCarga{
 		fclose($f);
 		fclose($resp);
 		
-		
+		$xml .= "</Ruts>";
 		
 		/*$arrRut .= '</Resp>';
 		fwrite($log, "$arrRut\r\n");
@@ -217,88 +223,93 @@ class XbrlCarga{
 
 
 		
-		$arrRut .= '</Resp>';
+		
 		fwrite($log, "$arrRut\r\n");
-		fclose($log);
+		
+		fwrite($log, "$xml\r\n");
+		
+		
 		
 		//return '<Resp><rut>19</rut><rut>27</rut></Resp>';
+		
+		
+		try { 
+			//$con = new PDO('sqlsrv:Server=WOTAN-PC;Database=agf');	 
+			$con = new PDO('sqlsrv:Server=MFUENTEALBA\WOTAN;Database=agf');			
+							
+			try {
+		        $con->beginTransaction(); 
+				$sql = "exec sp_xbrl_extraccion ?";
+				
+				$sql2 = str_replace("'", "''", $sql);
+				$stmtlog = $con->prepare("INSERT INTO logs values ('" . $sql2 . "');");
+				$stmtlog->execute();
+				$stmt = $con->prepare($sql);				
+				$stmt->bindParam(1, $xml); 
+				$stmt->execute();
+				$con->commit();
+			} catch(PDOExecption $e) {
+		        $con->rollback();
+		        print "Error!: " . $e->getMessage() . "</br>";
+				return "bien";//$xmlParam->xmlParam;
+		    } 			
+		} catch( PDOExecption $e ) {
+		    print "Error!: " . $e->getMessage() . "</br>";
+			return "mal 1";//$xmlParam->xmlParam;
+		} 	
+		
+		fwrite($log, count($rutsProcesados) . "-cantidad\r\n");
+		for($i = 0; $i < count($rutsProcesados); $i++){
+			$ArchivoRemoto = "http://www.svs.cl/institucional/mercados/consulta_busqueda.php?entidad_web=G&valor=" . $rutsProcesados[$i];
+			
+			$useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
+			$ch = curl_init();
+			// set user agent
+			curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+			curl_setopt ($ch, CURLOPT_URL, $ArchivoRemoto);
+			curl_setopt ($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			
+			$datos = curl_exec ($ch);
+			
+			curl_close ($ch);
+			//fwrite($log, print_r($datos, true) . "\r\n");
+			//$datos = htmlspecialchars($datos);
+			$datos = explode("\n", $datos);
+			fwrite($log, count($datos) . "------------------cantidad\r\n");
+			//fwrite($log, print_r($datos, true) . "\r\n");
+			$registro = new stdClass();	
+			$registro->rut = trim(strip_tags($datos[120]));
+			$registro->nombre = trim(strip_tags($datos[122]));
+			$registro->tipoEntidad = trim(strip_tags($datos[123]));
+			$registro->vigencia = trim(strip_tags($datos[124]));
+			$arrRut .= '<rut id="' . $registro->rut . '" nombre="' . $registro->nombre . '" tipoEntidad="' . $registro->tipoEntidad . '" vigencia="' . $registro->vigencia . '"/>';
+			fwrite($log, print_r($registro, true) . "\r\n");
+
+		}
+		
+		$arrRut .= '</Resp>';
+		
+
+		unset($con); 
+		unset($stmt);
+		fclose($log);
+		
 		return $arrRut;
 	}
 	
 	public function descargaSeleccion($data){
-		$dat = fopen("xbrl\\" . "DATA.txt", "r");
-		$re =  fopen("xbrl\\" . "DATARE.txt", "w");
-		$log = fopen("xbrl\\" . 'log.txt', 'a');
-		//$ruts = stdClass();
-		$ruts = array();
-		$data = explode(";", $data);
-		
-		for($i = 0; $i < count($data); $i++){
-			$ruts[$data[$i]] = $data[$i];
-		}
-		
-		fwrite($log, print_r($ruts, true) . "\r\n");
-		
-		
-		while(!feof($dat)){
-			
-			$xbrl = fgets($dat);
-			
-			$rut = explode("rut=", $xbrl);
-			$rut = explode("&mm", $rut[1]);
-			
-			if(@isset($ruts[$rut])){
-				$xbrl = str_replace(" ", "%20", $xbrl);
-				$xbrl = trim($xbrl);	
-				fwrite($log, "-------------------------------------------------------\r\n");
-				fwrite($log, "procesando: " . $xbrl ."\r\n");
-				$líneas = file($xbrl);
-				$i = 0;
-				foreach ($líneas as $num_línea => $línea) {
-					//echo "Línea #<b>{$num_línea}</b> : " . htmlspecialchars($línea) . "<br />\n";
-					$i++;
-				}
-				if($i < 2){
-					$xbrl = str_replace(".zip", ".xbrl", $xbrl);
-					fwrite($re, $xbrl . "\r\n");
-					fwrite($log, "ERROR\r\n");
-				} else {
-					//$xbrl = fgets($dat);
-					fwrite($log, "CORRECTO\r\n");
-					$xbrl = explode("&", $xbrl);
-					$xbrl = explode("=", $xbrl[5])[1];
-					fwrite($log, $xbrl ."\r\n");
-					file_put_contents($xbrl, $líneas)
-					or die("<br />No se puede escribir el archivo local<br />");
-				}
-			}
-			
-			
-			
-		}
-		fclose($dat);
-		fclose($re);
+		$log2 = fopen('logZZZZZ11.txt', 'a');
+		fwrite($log2, "$data\r\n");
+		fclose($log2);
 
-		$dat =  fopen("xbrl\\" . "DATARE.txt", "r");
-
-		while(!feof($dat)){
-			$xbrl = fgets($dat);
-			$xbrl = str_replace(" ", "%20", $xbrl);
-			$xbrl = trim($xbrl);	
-			fwrite($log, "-------------------------------------------------------\r\n");
-			fwrite($log, "procesando: " . $xbrl ."\r\n");
-			
-			$líneas = file($xbrl);
-			$xbrl = explode("&", $xbrl);
-			$xbrl = explode("=", $xbrl[5])[1];
-			fwrite($log, $xbrl ."\r\n");
-			file_put_contents($xbrl, $líneas)
-			or die("<br />No se puede escribir el archivo local<br />");
-		}
-
-		fclose($dat);
+		$salida = shell_exec("C:\\xampp\\php\\php.exe C:\\xampp\\htdocs\\ProcesaXml2\\procesoRecursivo.php " . $data);
+		//shell_exec("..\\..\\php5\\php.exe ..\\recursivo.php 0 0 0 0");
+		$log = fopen('xbrl\\log.txt', 'a');
+		//fwrite($log, $salida . "\r\n");
 		fclose($log);
-		return true;
+		$salida = explode(";;;;;;;", $salida);
+		return $salida;
 	}
 	
 	public function manipulaZip($data){
