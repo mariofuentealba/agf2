a408d574eeacca46991722bfb9a30a3ae34c04e6
mariofuentealba <anti.demiurgo@gmail.com>
2014-06-27 11:40:55 -0400

    Update sp_xbrl_insertar_valores.sql

--- servicios/sp_xbrl_insertar_valores.sql
+++ servicios/sp_xbrl_insertar_valores.sql
@@ -1,6 +1,6 @@
 USE [agf]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_xbrl_insertar_valores]    Script Date: 06/25/2014 19:18:42 ******/
+/****** Object:  StoredProcedure [dbo].[sp_xbrl_insertar_valores]    Script Date: 06/27/2014 11:30:40 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -25,6 +25,10 @@ declare @rut varchar(10)
 declare @validaRut varchar(10)
 declare @periodo varchar(10)
 declare @idPer int
+declare @fechaActual datetime
+
+
+select @fechaActual = sysdatetime()
 
 
 select @rut = NODO.ITEM.value('@rut','VARCHAR(10)'), @periodo = NODO.ITEM.value('@periodo','VARCHAR(10)')
@@ -44,12 +48,22 @@ DECLARE @tag TABLE (
 INSERT INTO @tag (idTag, valor, contextRef) 
 SELECT NODO.ITEM.value('@idTag','VARCHAR(50)'), NODO.ITEM.value('@valor','VARCHAR(50)'), NODO.ITEM.value('@contextRef','VARCHAR(100)')
 FROM   @xmlParam.nodes('/Tags/tag') AS NODO(ITEM)
-
+where NODO.ITEM.value('@idTag','VARCHAR(50)') = 'Assets'
 
 
 select @idPer = ID_PERIODO
 from periodos
 where label = @periodo
+
+
+update v 
+set valor = a.valor,
+	[DT_MODIFICACION] = @fechaActual
+from @tag a  
+	inner join xbrl_tag b on a.idTag = b.tag
+	inner join xbrl_contexto a1 on a1.nombre = a.contextRef and a1.id_periodo = @idPer
+	inner join [agf].[dbo].[valoresResp] v on v.ID_TAG_AGF = b.id and v.tipo = a1.id and  @validaRut = v.ID_EMPRESA and @idPer = v.ID_PERIODO and 1 = v.origen
+
 
 INSERT INTO [agf].[dbo].[valoresResp]
            ([ID_TAG_AGF]
@@ -61,11 +75,13 @@ INSERT INTO [agf].[dbo].[valoresResp]
            ,[origen]
            ,[id_formula]
            )
-select  b.id as id_tag, @validaRut, @idPer, a1.id as id_contexto, a.valor, sysdatetime(), 1, 0
+select  b.id as id_tag, @validaRut, @idPer, a1.id as id_contexto, a.valor, @fechaActual, 1, 0
 from @tag a 
 	inner join xbrl_tag b on a.idTag = b.tag
-	inner join xbrl_contexto a1 on a1.nombre = a.contextRef
-
+	inner join xbrl_contexto a1 on a1.nombre = a.contextRef and a1.id_periodo = @idPer
+except
+select [ID_TAG_AGF],[ID_EMPRESA],[ID_PERIODO],[tipo],[VALOR],@fechaActual,[origen],[id_formula]
+from [agf].[dbo].[valoresResp]
 
 
 
