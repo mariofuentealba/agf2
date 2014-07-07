230de0d0b04c411cf7e91829945169c3a5262084
mariofuentealba <anti.demiurgo@gmail.com>
2014-07-06 23:29:47 -0400

    Update creacionEstructuraXBRL.sql

--- servicios/creacionEstructuraXBRL.sql
+++ servicios/creacionEstructuraXBRL.sql
@@ -5814,6 +5814,46 @@ CREATE TABLE [dbo].[log_error_carga](
 )
 
 
+drop TABLE [dbo].[indices_financieros]
+
+/****** Object:  Table [dbo].[indices_financieros]    Script Date: 07/06/2014 18:12:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[indices_financieros](
+	[ID_INDICE_FINANCIERO] [int] IDENTITY(1,1) NOT NULL,
+	[ID_GRUPO_INDICE_FINANCIERO] [int] NULL,
+	[NOMBRE] [varchar](300) NULL,
+	[ID_FORMULA] [int] NULL,
+	[OA] [varchar](1) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID_INDICE_FINANCIERO] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) 
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[indices_financieros] ADD  DEFAULT (NULL) FOR [ID_GRUPO_INDICE_FINANCIERO]
+GO
+
+ALTER TABLE [dbo].[indices_financieros] ADD  DEFAULT (NULL) FOR [NOMBRE]
+GO
+
+ALTER TABLE [dbo].[indices_financieros] ADD  DEFAULT (NULL) FOR [ID_FORMULA]
+GO
+
+ALTER TABLE [dbo].[indices_financieros] ADD  DEFAULT (NULL) FOR [OA]
+GO
+
 
 
 
